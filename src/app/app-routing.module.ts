330d2f407c0ee0d67338e74eddcd527343a9b1d7
hqushtom1 <86080933+hqushtom1@users.noreply.github.com>
2022-03-23 11:31:58 -0400

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  { path: "", component: StudentComponent },
+  { path: "**", redirectTo: "/" }
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
